fb6feeb8f0d2c24e2e935de23ac50c08fbd16669
Alolaxus <billyzinna@gmail.com>
2017-06-11 00:16:30 +0200

    'bll'

--- Second.py
+++ Second.py
@@ -1,9 +1,15 @@
 import requests
 import json
 import time
-
+import random
+import telegram
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+import logging
+import re
+from uuid import uuid4
 
 URL = "https://api.telegram.org/bot322854984:AAG34-oiQAUW2tpu3JDtkSaUnHPzf8xhqO0/"
+
 
 def get_url(url):
     response = requests.get(url)
@@ -16,12 +22,14 @@ def get_json_from_url(url):
     js = json.loads(content)
     return js
 
+
 def get_updates(offset=None):
     url = URL + "getUpdates"
     if offset:
         url += "?offset={}".format(offset)
     js = get_json_from_url(url)
     return js
+
 
 def get_last_chat_id_and_text(updates):
     num_updates = len(updates["result"])
@@ -30,14 +38,66 @@ def get_last_chat_id_and_text(updates):
     chat_id = updates["result"][last_update]["message"]["chat"]["id"]
     return (text, chat_id)
 
+
 def send_message(text, chat_id):
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
 
-text, chat =   get_last_chat_id_and_text(get_updates())
-if text == "come va":
-    send_message("bene", chat)
-if text == "suca":
-    send_message("milla", chat)
-else:
-    send_message("non ho capito", chat)
+
+text, chat = get_last_chat_id_and_text(get_updates())
+
+'''def main():
+    last_textchat = (None, None)
+    listConsigli = ["Apri un Blog!", "Non e' difficile, e' inutile!", "Prendi un dottorato!",
+                    "Non so aiutarti! Qui diventa cazzo!",
+                    "Paga le tue multe!"]
+
+
+    while True:
+        text, chat = get_last_chat_id_and_text(get_updates())
+        if (text, chat) != last_textchat:
+            if text == "come va":
+                send_message("bene", chat)
+            elif text == "suca":
+                send_message("milla", chat)
+            elif text == '/consiglio':
+                send_message(random.choice(listConsigli), chat)
+
+            else:
+                send_message("non ho capito", chat)
+
+            last_textchat = (text, chat)
+        time.sleep(0.5)
+
+
+if __name__ == '__main__':
+        main()
+        '''
+
+
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+
+
+def consiglio(bot, update):
+    listConsigli = ["Apri un Blog!", "Non e' difficile, e' inutile!", "Prendi un dottorato!",
+                    "Non so aiutarti! Qui diventa cazzo!",
+                    "Paga le tue multe!"]
+    update.message.reply_text("Ciao!")
+
+
+def main():
+    updater = Updater("322854984:AAG34-oiQAUW2tpu3JDtkSaUnHPzf8xhqO0")
+    bot = telegram.Bot(token='322854984:AAG34-oiQAUW2tpu3JDtkSaUnHPzf8xhqO0')
+    print(bot.get_me())
+
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("consiglio", consiglio))
+    dp.add_handler(CommandHandler("start", start))
+    updater.start_polling()
+
+    updater.idle()
+
+    if __name__ == '__main__':
+        main()
