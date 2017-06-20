fa8d75ed303b1b4db8e7893d3cc042e7f26f7824
Alolaxus <billyzinna@gmail.com>
2017-06-20 20:45:32 +0200

    '0.9'

--- Main.py
+++ Main.py
@@ -112,10 +112,11 @@ def gatti(bot, update):
     listfotogatti = blue.listfotogatti
 
 
-    
+
     chat_id =  update.message.chat_id
     bot.send_message(chat_id=chat_id, text=random.choice(listgatti))
     bot.send_video(chat_id=chat_id, video="http://thecatapi.com/api/images/get?format=src&type=gif")
+    print(chat_id)
 
     '''random.choice(listfotogatti)'''
 
