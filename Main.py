2097603753c4f5e443184958d9350023fbdc24d9
Alolaxus <billyzinna@gmail.com>
2017-06-20 19:15:56 +0200

    '0.9'

--- Main.py
+++ Main.py
@@ -113,7 +113,7 @@ def gatti(bot, update):
 
 
 
-    chat_id = bot.get_updates(offset=1).message.chat_id
+    chat_id = bot.get_updates().message.chat_id
     bot.send_message(chat_id=chat_id, text=random.choice(listgatti))
     bot.send_video(chat_id=chat_id, video="http://thecatapi.com/api/images/get?format=src&type=gif")
 
