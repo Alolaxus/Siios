44fdbfc8f3395d29ca9f77258e5db80344062044
Alolaxus <billyzinna@gmail.com>
2017-06-22 11:28:21 +0200

    '1.01'

---
+++ watsontts.py
@@ -0,0 +1,38 @@
+import requests
+
+import copy
+from urllib.parse import urlencode
+
+syn_parameters = {
+    'text': '',
+    'voice': 'en-US_AllisonVoice',
+    'download': True,
+    'accept': 'audio/ogg'
+}
+
+
+def main():
+    # Get the text
+    text = input('Text: ')
+
+    # Synthesize it
+    s = synthesize(text)
+
+    # Write it to a `f.ogg` file, overwriting it if it exists
+    with open('f.ogg', 'wb+') as f:
+        f.write(s.content)
+
+
+def synthesize(text):
+    # Don't modify the original object
+    parameters = copy.copy(syn_parameters)
+
+    # Set the text
+    parameters['text'] = text
+
+    # Request foo!
+    r = requests.get('https://text-to-speech-demo.mybluemix.net/api/synthesize?' + urlencode(parameters))
+
+    return r
+
+
